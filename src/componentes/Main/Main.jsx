e6b18e14f003854112f3e7d76246d403f442658f
Mevisheshkumar <visheshkumar750000@gmail.com>
2024-11-11 10:14:42 +0530

    some addon

--- src/componentes/Main/Main.jsx
+++ src/componentes/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
     return (
         <div className='main'>
             <div className="nav">
-                <p>Ask Me</p>
+                <p>Generative AI</p>
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-contener">
@@ -15,20 +15,33 @@ const Main = () => {
                 </div>
                 <div className="cards">
                     <div className="card">
-                        <p>text1gdfhghjdfjhgdfjgjdfgj</p>
+                        <p>text1</p>
                         <img src={assets.compass_icon} alt="" />
                     </div>
                     <div className="card">
-                        <p>text2dgdfgdfghdfhgkjdfhgkdf</p>
+                        <p>text2</p>
                         <img src={assets.bulb_icon} alt="" />
                     </div>
                     <div className="card">
-                        <p>text3gjdfhkgjhdfkghkldf</p>
+                        <p>text3</p>
                         <img src={assets.message_icon} alt="" />
                     </div>
                     <div className="card">
-                        <p>text4gkjhsdfjkhgkhdfkghdfklghkl</p>
+                        <p>text4</p>
                         <img src={assets.code_icon} alt="" />
+                    </div>
+                    <div className="main-bottom">
+                        <div className="search-box">
+                            <input type="text" placeholder="Enter prompt" />
+                            <div>
+                                <img src={assets.gallery_icon} alt="" />
+                                <img src={assets.mic_icon} alt="" />
+                                <img src={assets.send_icon} alt="" />
+                            </div>
+                        </div>
+                        <p className="bottom-info">
+                            text you want ?
+                        </p>
                     </div>
                 </div>
             </div>
